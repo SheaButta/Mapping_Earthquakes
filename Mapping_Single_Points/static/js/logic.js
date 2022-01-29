00fa12c7d96828e4d8a9367c3807da63211eaf89
Shea T Walker <madmax_113@hotmail.com>
2022-01-29 12:41:53 -0500

    Updated file @1241/1/29/2022

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -24,6 +24,5 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-
-// Then we add our 'graymap' tile layer to the map.
+// Then we add our 'graymap' tile layer to the map.  TEST
 streets.addTo(map);
